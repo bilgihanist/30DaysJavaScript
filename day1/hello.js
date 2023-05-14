d04137a6a2bed459f2ac2a71e808455ac70e7652
bilgihanist <71718784+bilgihanist@users.noreply.github.com>
2023-05-14 21:20:06 +0300

    first commit

---
+++ day1/hello.js
@@ -0,0 +1,29 @@
+console.log("hello deneme")
+
+let firstName11
+console.log(firstName11)
+
+let emptyValue = null
+console.log(emptyValue)
+
+console.log(typeof 'bilgihan')
+console.log(typeof 5)
+console.log(typeof true)
+console.log(typeof null)
+console.log(typeof undefined)
+
+
+
+let amyage
+var boy
+let deneme
+let manyak
+console.log(amyage,boy,deneme,manyak)
+
+
+let boyum = 1.80
+let kilom = 80
+const pipim = 45
+var popom = 90
+console.log("boyum: "+boyum,"kilom: "+kilom,"pipim: "+pipim,"popom: "+popom)
+asdasd
---
+++ day1/intro.js
@@ -0,0 +1,28 @@
+console.log("naber kuzu");
+
+let age = 199;
+const gravity = 9.11;
+const point = 88;
+const pi = 3.14;
+console.log(gravity, point, pi, age);
+
+let name = "ziya",
+  job = "önyüzcü",
+  live = "istanbul",
+  age2 = 25;
+console.log(name, job, live, age2);
+
+let firstName = "ziyacan",
+  lastName = "can",
+  maritalStatus = "single",
+  country = "turkey",
+  sex = "womennn",
+  yasin = 28;
+
+console.log(firstName, lastName, maritalStatus, country, sex, yasin);
+
+
+let mayAge = 25
+const yourAge = 30
+console.log("I am "+mayAge+" years old")
+console.log("I am "+yourAge+" years old")
